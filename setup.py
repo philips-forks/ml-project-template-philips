26835e127234f135beba7666c52f50fd4b2f67c6
Artem Lobantsev <lobantseff@gmail.com>
2021-10-29 00:46:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
+from pathlib import Path
 from setuptools import setup, find_packages
-
 setup(
-    name="template-ml-project",
+    name=Path(__file__).parent.name,
     version="0.0.1",
-    packages=find_packages(include=["template_ml_project"]),
+    packages=find_packages(where=["src"]),
 )
