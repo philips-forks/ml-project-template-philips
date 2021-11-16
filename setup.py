c90e057843f9a4c2f09c4611427064e378947381
Artem Lobantsev <lobantseff@gmail.com>
2021-11-16 15:41:40 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name=Path(__file__).parent.name,
     version="0.0.1",
-    packages=find_packages(where=["src"]),
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
 )
