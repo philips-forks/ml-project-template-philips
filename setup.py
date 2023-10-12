8cf2afe6b5687aa66132dbac1ffad91e48383eff
Artem Lobantsev <lobantseff@gmail.com>
2023-10-12 20:08:54 +0200

    Update setuptools

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
-from pathlib import Path
-from setuptools import setup, find_packages
-setup(
-    name=Path(__file__).parent.name,
-    version="0.0.1",
-    package_dir={"": "src"},
-    packages=find_packages(where="src"),
-)
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
